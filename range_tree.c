9f8ad8082190793aab52fd02c35660edfae94008
Logan Mahan <mahan@royal-16.cs.wisc.edu>
2018-11-29 18:11:10 -0600

    More and more updates!!!

--- range_tree.c
+++ range_tree.c
@@ -9,6 +9,7 @@ typedef struct TreeNode {
     struct TreeNode* parent;
     struct TreeNode* left;
     struct TreeNode* right;
+    int height;
 }TreeNode;
 
 TreeNode* createNode(void* ptr, size_t size, TreeNode* parent) {
@@ -24,12 +25,61 @@ TreeNode* createNode(void* ptr, size_t size, TreeNode* parent) {
     node->parent = parent;
     node->left = NULL;
     node->right = NULL;
+    node->height = 1;
     return node;
 }
 
+// Don't use anymore.
 TreeNode* createTree(void* ptr, size_t size) {
     return createNode(ptr, size, NULL);
 }   
+
+int max(int a, int b) {
+	if (a > b) return a;
+	else return b;
+}
+
+/*
+ * This method returns the balance factor between the two subtrees rooted at the given node.
+ */
+int balance(TreeNode* node) {
+    if (node == NULL) {
+        return 0;
+    }
+    return node->left->height - node->right->height;
+}
+
+/*
+ * This method performs a left rotate on a subtree with TreeNode root as its root node.
+ */
+TreeNode* rotateLeft(TreeNode* node) {
+    TreeNode* a = node->right;
+    TreeNode* b = a->left;
+
+    a->left = node;
+    node->right = b;
+
+    node->height = max(node->left->height, node->right->height) + 1;
+    a->height = max(a->left->height, a->right->height) + 1;
+
+    return a;
+}
+
+/*
+ * This method performs a right rotate on a subtree with TreeNode root as its root node.
+ */
+TreeNode* rotateRight(TreeNode* node) {
+    TreeNode* a = node->left;
+    TreeNode* b = a->right;
+
+    a->right = node;
+    node->left = b;
+
+    node->height = max(node->left->height, node->right->height) + 1;
+    a->height = max(a->left->height, a->right->height) + 1;
+
+    return a;
+}
 
 /*
  * This method evaluates whether or not there is overlap in the tree between two nodes.
@@ -55,17 +105,32 @@ int overlapCheck(TreeNode* node1, TreeNode* node2) {
     return 0;
 }
 
+void maxUpdate(TreeNode* node) {
+    if (node->left != NULL && node->left->max > node->max) {
+	node->max = node->left->max;
+    }
+    if (node->right != NULL && node->right->max > node->max) {
+	node->max = node->right->max;
+    }
+}
+
+
 /*
  * This method inserts a TreeNode into a given tree, updates the max value in the tree if needed,
  * and checks for overlap in the tree.
  */
-void insertNode(TreeNode* tree, void* ptr, size_t size) {
-    if(tree == NULL){
-	tree = createTree(ptr, size);
+void insertNode(TreeNode* tree, void* ptr, size_t size, TreeNode* parent) {
+    // Print error statement here?
+    if(ptr == NULL){
+	fprintf(stderr, "Null pointer passed in.");
 	return;
     }
 
-    TreeNode* newNode = NULL;  // This is a placeholder for the new node being added to the tree.
+    if(tree == NULL){
+	tree = createNode(ptr, size, parent);
+    }
+
+/*    TreeNode* newNode = NULL;  // This is a placeholder for the new node being added to the tree.
     TreeNode* currNode = tree; // This keeps track of the current node being iterated on.
     
     // call balance after setting currNode's children?
@@ -79,6 +144,7 @@ void insertNode(TreeNode* tree, void* ptr, size_t size) {
 	    if(currNode->left == NULL) {
 	        newNode = createNode(ptr, size, currNode);
 	        currNode->left = newNode;
+		currNode->left->max = newNode->high;
 	    } else {
 		currNode = currNode->left;
 	    }
@@ -100,9 +166,50 @@ void insertNode(TreeNode* tree, void* ptr, size_t size) {
     	    }
 	}	    
     }
+*/
+    // Update max value
+    if (ptr + size > tree->max) {
+	tree->max = ptr + size;
+    }
+
+    if (ptr < tree->low) {
+	
+	insertNode(tree->left, ptr, size, tree);
+    } else if (ptr > tree->low) {
+	insertNode(tree->right, ptr, size, tree);
+    // In the case that the ptrs are equal
+    } else {
+	fprintf(stderr, "Tried to insert two pointers of the same value.");
+        return;
+    }
+
+    tree->height = max(tree->left->height, tree->right->height) + 1;
+
+    int bal = balance(tree);
+    
+    // Left Left    
+    if (bal > 1 && ptr < tree->left->low) {
+	rotateRight(tree);
+    }
+
+    // Right Right
+    if (bal < -1 && ptr > tree->right->low) {
+	rotateLeft(tree);
+    }
+
+    // Left Right
+    if (bal > 1 && ptr > tree->left->low) {
+	rotateLeft(tree->left);
+	rotateRight(tree);
+    }
+
+    // Right Left
+    if (bal < -1 && ptr < tree->right->low) {
+	rotateRight(tree->right);
+	rotateLeft(tree);
+    }
 
     return;
-    // implement shifting to ensure balanced tree
 }
 
 void removeNode(TreeNode* tree, void* ptr, size_t size) {
--- range_tree.h
+++ range_tree.h
@@ -5,6 +5,9 @@ typedef struct TreeNode TreeNode;
 
 TreeNode* createTree(void*, size_t);
 int overlapCheck(TreeNode*, TreeNode*);
+int balance(TreeNode*);
+TreeNode* rotateRight(TreeNode);
+TreeNode* rotateLeft(TreeNode*);
 void insertNode(TreeNode*, void*, size_t);
 void removeNode(TreeNode*, void*, size_t);
 
